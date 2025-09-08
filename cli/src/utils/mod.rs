9aea058bc64e9c72ed6af95f06273fa7dbdaaca2
Galen <goshea@me.com>
2025-09-08 08:23:26 -0700

    chore: remove unused functions to fix build warnings [skip-e2e]

--- cli/src/utils/mod.rs
+++ cli/src/utils/mod.rs
@@ -7,18 +7,6 @@ use colored::Colorize;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::time::Duration;
 
-pub fn create_progress_bar(len: u64, message: &str) -> ProgressBar {
-    let pb = ProgressBar::new(len);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template("{spinner:.green} {msg} [{bar:40.cyan/blue}] {pos}/{len} ({eta})")
-            .unwrap()
-            .progress_chars("#>-"),
-    );
-    pb.set_message(message.to_string());
-    pb
-}
-
 pub fn create_spinner(message: &str) -> ProgressBar {
     let pb = ProgressBar::new_spinner();
     pb.set_style(
